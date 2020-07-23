4ff86662d42376e1b2def3a535e3033f95b622db
Lieke van Son <angelieke.van_son@cfa.harvard.edu>
2020-07-23 06:01:48 -0400

    added python scripts to run grids of sw on cluster (in copies of files to not mess with main code)

--- output/slurms/slurm_0.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --mem-per-cpu=1024
-#SBATCH --time=1:00:00
-#SBATCH --output=output.out
-/n/home04/lvanson/Programs/COMPAS/src/COMPAS --grid /n/home04/lvanson/Programs/stroopwafel/output/grid_0.csv --outputPath /n/home04/lvanson/Programs/stroopwafel/output --logfile-delimiter COMMA --output-container batch_0 --random-seed 1614846107291031811 > /n/home04/lvanson/Programs/stroopwafel/output/logs/log_0.txt 
