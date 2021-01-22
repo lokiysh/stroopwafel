45e1e829c13393c51202ac4fb7dbef1566eca46e
Floris Kummer <floris.kummert@gmail.com>
2021-01-22 11:34:08 +0100

    added comments to constants.py

--- stroopwafel/constants.py
+++ stroopwafel/constants.py
@@ -1,8 +1,9 @@
 """
-This file will contain all the constants
+This file will contain all the necessary constants
 """
-ALPHA_IMF = -2.3
+ALPHA_IMF = -2.3                        # power-law index initial mass function
 
+# used for calculating ZAMS radii
 R_COEFF =\
     [[1.71535900,    0.62246212,     -0.92557761,    -1.16996966,    -0.30631491],\
     [6.59778800,    -0.42450044,    -12.13339427,   -10.73509484,   -2.51487077],\
@@ -15,10 +16,11 @@ R_COEFF =\
     [0.00022582,    -0.00186899,    0.00388783,     0.00142402,     -0.00007671]]
 MINIMUM_SECONDARY_MASS = 0.1
 R_SOL_TO_AU = 0.00465047
+# why is there a difference between metallicity_sol and Zsol?
 METALLICITY_SOL = 0.0142
 ZSOL = 0.02
-REJECTION_SAMPLES_PER_BATCH = 1e4
-TOTAL_REJECTION_SAMPLES = 1e6
-NUM_GENERATIONS = 10
-MIN_ENTROPY_CHANGE = 0.01
-KAPPA = 1.0
+REJECTION_SAMPLES_PER_BATCH = 1e4        # test value to calculate rejection rate
+TOTAL_REJECTION_SAMPLES = 1e6            # ditto
+NUM_GENERATIONS = 10                     # number of GenAIS adaptation phases
+MIN_ENTROPY_CHANGE = 0.01                # minimum improvement necessary to switch between generations
+KAPPA = 1.0                              # adapted distribution width multiplier
